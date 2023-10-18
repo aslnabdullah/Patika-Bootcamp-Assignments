a51e08489fa0c7ef2af0719fe8d71d71c9409377
abdullahaslan <abdullahaslan2510@gmail.com>
2023-10-18 15:55:20 +0300

    new commit

---
+++ Java-Assignments/src/Week2/FindPallindromeNumber.java
@@ -0,0 +1,28 @@
+package Week2;
+
+import java.util.Scanner;
+
+public class FindPallindromeNumber {
+    public static void main(String[] args) {
+        boolean control = false;
+        System.out.println("lütfen kontrol etmek istediğiniz kelimeyi giriniz.");
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        if (isPallindrome(word)){
+            System.out.println("girdiğiniz " + word + " kelimesi pallindrome kelimedir.");
+        }
+        else {
+            System.out.println("Girdiğiniz " +word + " kelimesi pallindrome değildir");
+        }
+
+
+    }
+    static boolean isPallindrome(String str){
+        String reverse = "";
+        for (int i = str.length()-1; i>=0;i--){
+            reverse += str.charAt(i);
+
+        }
+        return str.equals(reverse);
+    }
+}
